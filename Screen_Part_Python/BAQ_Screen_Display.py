3b87cb4a54d6b1f00d52d2f298e482a8b595ca9f
Harish Ram Nambiappan <harishram.nambiappan@gmail.com>
2021-06-21 17:34:10 -0500

    Update BAQ_Screen_Display.py

--- Screen_Part_Python/BAQ_Screen_Display.py
+++ Screen_Part_Python/BAQ_Screen_Display.py
@@ -1,22 +1,6 @@
 from NewsScreenDisplay.DynamoDBData import DDBRetrieve
 from NewsScreenDisplay.NewsDisplay import Display
 
-'''
-def screen_display(url):
-    app = QApplication(sys.argv)
-
-    wid = QWidget()
-    wid.resize(1000, 1000)
-
-    news = QWebEngineView(wid)
-    news.resize(1000, 1000)
-    news.load(QUrl(url))
-    news.show()
-
-    wid.show()
-
-    sys.exit(app.exec_())
-'''
 while True:
     news_retrieve = DDBRetrieve()
     news_url = news_retrieve.command_retrieve('AWS_Verizon_Commands')
